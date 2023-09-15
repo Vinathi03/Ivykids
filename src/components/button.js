8f6e4e3211a2f7a83263924e34e07beb88d747af
Vinathi03 <110575443+Vinathi03@users.noreply.github.com>
2023-09-15 23:30:46 +0530

    Delete src directory

--- src/components/button.js
+++
@@ -1,9 +0,0 @@
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css';
-function MyButton() {
-    return (
-      <Button variant="primary">I'm a button</Button>
-    );
-  }
-
-export default MyButton;  
